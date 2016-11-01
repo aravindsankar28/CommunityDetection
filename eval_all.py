efa7027d93fad1907b2ec5afc1db0540c79c5d42
Aravind Sankar <aravindsankar28@gmail.com>
2016-10-31 22:29:46 -0500

    Better eval code

---
+++ eval_all.py
@@ -0,0 +1,126 @@
+def eval_jaccard(predFile,groundTruthFile):
+
+	pred = []
+	with open(predFile) as f:
+		lines = f.read().splitlines()
+		for line in lines:
+			split = line.split()
+			community = []
+			for x in split[1:len(split)]:
+				community.append(int(x))
+			pred.append(community)
+
+	
+	actual = []
+
+	with open(groundTruthFile) as f:
+		lines = f.read().splitlines()
+		for line in lines:
+			split = line.split()
+			community = []
+			for x in split[1:len(split)]:
+				community.append(int(x))
+			actual.append(community)
+
+
+	score1 = 0.0
+	score2 = 0.0
+
+
+	for i in pred:
+		max_i = 0
+		for j in actual:
+			jaccard_sim = len(set(i).intersection(set(j)))*1.0/ len(set(i).union(set(j)))
+			if max_i < jaccard_sim:
+				max_i = jaccard_sim
+		score1 += max_i
+
+
+	score1 /= (2.0 * len(pred))
+
+	for i in actual:
+		max_i = 0
+		for j in pred:
+			jaccard_sim = len(set(i).intersection(set(j)))*1.0/ len(set(i).union(set(j)))
+			if max_i < jaccard_sim:
+				max_i = jaccard_sim
+		score2 += max_i
+
+
+	score2 /= (2.0 * len(actual))
+
+	score = score1 + score2
+	return score
+
+
+def eval_f1(predFile, groundTruthFile):
+	
+
+	pred = []
+	with open(predFile) as f:
+		lines = f.read().splitlines()
+		for line in lines:
+			split = line.split()
+			community = []
+			for x in split[1:len(split)]:
+				community.append(int(x))
+			pred.append(community)
+
+	
+	actual = []
+
+	with open(groundTruthFile) as f:
+		lines = f.read().splitlines()
+		for line in lines:
+			split = line.split()
+			community = []
+			for x in split[1:len(split)]:
+				community.append(int(x))
+			actual.append(community)
+
+
+	score1 = 0.0
+	score2 = 0.0
+
+	for i in pred:
+		max_i = 0
+		for j in actual:
+			p =len(set(i).intersection(set(j)))*1.0/ len(set(i))
+			r =len(set(i).intersection(set(j)))*1.0/ len(set(j))
+			if p > 0.0 and r >0.0:
+				f1 = 2*p*r/(p+r)
+			else:
+				f1 = 0.0
+			if max_i < f1:
+				max_i = f1
+		score1 += max_i
+
+
+	score1 /= (2.0 * len(pred))
+
+	for i in actual:
+		max_i = 0
+		for j in pred:
+			p =len(set(i).intersection(set(j)))*1.0/ len(set(i))
+	        r =len(set(i).intersection(set(j)))*1.0/ len(set(j))
+	        if p>0.0 and r > 0.0:
+	        	f1 = 2*p*r/(p+r)
+	        else:
+	        	f1 = 0.0
+	        if max_i < f1:
+	        	max_i = f1
+		score2 += max_i
+
+
+	score2 /= (2.0 * len(actual))
+
+	score = score1 + score2
+	return score
+
+
+with open('facebook/filenames.txt') as f: 
+	lines = f.read().splitlines()
+	for f in lines:
+		p1 = "facebook/"+f
+		p2 = "PLSA_Network/Results/"+f
+		print eval_jaccard(p1,p2), eval_f1(p1,p2)
