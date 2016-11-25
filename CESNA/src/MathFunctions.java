3506e2f17efc18674d47c093f75f9a11921f48fb
Aravind Sankar <aravindsankar28@gmail.com>
2016-11-24 20:59:12 -0600

    Change to arrays

--- CESNA/src/MathFunctions.java
+++ CESNA/src/MathFunctions.java
@@ -14,6 +14,20 @@ public class MathFunctions {
 		return sum;
 	}
 
+	static double dotProduct(double[] x, double[] y) {
+		double sum = 0.0;
+		if (x.length != y.length)
+			{
+				System.out.println("error");
+				return -1;
+			}
+
+		for (int i = 0; i < x.length; i++) {
+			sum += x[i] * y[i];
+		}
+		return sum;
+	}
+
 	static double L1Norm(HashMap<Integer, ArrayList<Double>> M) {
 		double s = 0.0;
 		for (int x : M.keySet()) {
@@ -23,11 +37,30 @@ public class MathFunctions {
 		return s;
 	}
 
+	static double L1Norm(double[][] M) {
+		double s = 0.0;
+		for (int x = 0; x < M.length; x++) {
+			for (int y = 0; y < M[x].length; y++)
+				s += Math.abs(M[x][y]);
+		}
+		return s;
+	}
+
 	static double L2NormSq(HashMap<Integer, ArrayList<Double>> M) {
 		double s = 0.0;
 		for (int x : M.keySet()) {
 			for (double y : M.get(x))
 				s += y * y;
+		}
+		return s;
+	}
+
+	static double L2NormSq(double[][] M) {
+		double s = 0.0;
+		for (int x = 0; x < M.length; x++) {
+			for (int y = 0; y < M[x].length; y++) {
+				s += M[x][y] * M[x][y];
+			}
 		}
 		return s;
 	}
--- CESNA/src/main.java
+++ CESNA/src/main.java
@@ -1,12 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class main {
-	HashMap<Integer, ArrayList<Integer>> G;
-	HashMap<Integer, ArrayList<Boolean>> X;
-	HashMap<Integer, ArrayList<Double>> F; // u,c
-	HashMap<Integer, ArrayList<Double>> W;// k,c
+	/*
+	 * HashMap<Integer, ArrayList<Integer>> G; HashMap<Integer,
+	 * ArrayList<Boolean>> X; HashMap<Integer, ArrayList<Double>> F; // u,c
+	 * HashMap<Integer, ArrayList<Double>> W;// k,c
+	 * 
+	 */
+	HashMap<Integer, Integer> nodeIdMap; // Code id to actual id
+	HashMap<Integer, Integer> nodeIdMapReverse; // Actual id to id in code.
+
+	ArrayList<ArrayList<Integer>> G; // Done
+	boolean[][] X; // Done
+	double[][] F;
+	double[][] W;
+
 	int C; // No of communities
 	int numAttr;
 	double alpha;
@@ -19,60 +30,90 @@ public class main {
 	int E;
 
 	public main(int com) {
+		nodeIdMap = new HashMap<>();
+		nodeIdMapReverse = new HashMap<>();
 		C = com;
 		numAttr = 0;
-		G = new HashMap<>();
-		X = new HashMap<>();
-		F = new HashMap<>();
-		W = new HashMap<>();
 		alpha = 0.5;
 		eta = 0.001;
 		lambda = 1;
 		maxIter = 1000;
 		THREADS = 4;
+		G = new ArrayList<>();
 	}
 
 	private void readGraph(String graphfilename, String attrfilename) throws Exception {
-		BufferedReader br = new BufferedReader(new FileReader(graphfilename));
+
+		BufferedReader br = new BufferedReader(new FileReader(attrfilename));
 		String line;
+		int nodeCounter = 0;
+		ArrayList<String> attributeStringList = new ArrayList<>();
 		while ((line = br.readLine()) != null) {
-			String[] edge = line.split(" ");
+			attributeStringList.add(line);
+		}
+		V = attributeStringList.size();
+		X = new boolean[V][];
+
+		for (String l : attributeStringList) {
+
+			String[] attributes = l.split(" ");
+			if (numAttr == 0) {
+				numAttr = attributes.length - 1;
+			}
+
+			int v = Integer.parseInt(attributes[0]);
+
+			if (!nodeIdMapReverse.containsKey(v)) {
+				nodeIdMap.put(nodeCounter, v);
+				nodeIdMapReverse.put(v, nodeCounter);
+				nodeCounter++;
+			}
+			int x = nodeIdMapReverse.get(v);
+			X[x] = new boolean[numAttr];
+
+			for (int i = 1; i < attributes.length; i++) {
+				X[x][i - 1] = Boolean.parseBoolean(attributes[i]);
+			}
+		}
+		br.close();
+		int V = X.length;
+
+		br = new BufferedReader(new FileReader(graphfilename));
+
+		ArrayList<String> edgeList = new ArrayList<>();
+
+		while ((line = br.readLine()) != null) {
+			edgeList.add(line);
+		}
+
+		for (int i = 0; i < V; i++)
+			G.add(new ArrayList<Integer>());
+
+		for (String l : edgeList) {
+			String[] edge = l.split(" ");
 			int e0 = Integer.parseInt(edge[0]);
 			int e1 = Integer.parseInt(edge[1]);
-			if (!G.containsKey(e0)) {
-				G.put(e0, new ArrayList<Integer>());
-			}
-			G.get(e0).add(e1);
+
+			int a = nodeIdMapReverse.get(e0);
+			int b = nodeIdMapReverse.get(e1);
+
+			G.get(a).add(b);
 			E++;
 		}
-		br.close();
-		br = new BufferedReader(new FileReader(attrfilename));
-		while ((line = br.readLine()) != null) {
-			String[] attributes = line.split(" ");
-			if (numAttr == 0)
-				numAttr = attributes.length - 1;
-			int v = Integer.parseInt(attributes[0]);
-			X.put(v, new ArrayList<Boolean>());
-			for (int i = 1; i < attributes.length; i++) {
-				X.get(v).add(Boolean.parseBoolean(attributes[i]));
-			}
-		}
+
 		br.close();
 		// adding back nodes that are in attrfile but not in graph
-		for (int node : X.keySet()) {
-			if (!G.containsKey(node)) {
-				G.put(node, new ArrayList<Integer>());
-			}
-		}
 		delta = -Math.log(1 - 1.0 / G.size());
-		V = G.size();
 		E /= 2;
+
+		W = new double[numAttr][C];
+		F = new double[V][C];
 	}
 
 	private void initAffiliations() {
 		// Compute the conductances of nodes
 		HashMap<Integer, Double> conductance = new HashMap<>();
-		for (int node : G.keySet()) {
+		for (int node = 0; node < G.size(); node++) {
 			ArrayList<Integer> neighbors = new ArrayList<Integer>();
 			neighbors.add(node);
 			for (int n : G.get(node)) {
@@ -112,15 +153,12 @@ public class main {
 				neighbors.add(n);
 			}
 			for (int n : neighbors) {
-				ArrayList<Double> Fu = new ArrayList<>();
 				for (int c = 0; c < C; c++) {
-					if (c == community) {
-						Fu.add(1 - bias + (bias * r.nextDouble()));
-					} else {
-						Fu.add(bias * r.nextDouble());
-					}
+					if (c == community)
+						F[n][c] = 1 - bias + (bias * r.nextDouble());
+					else
+						F[n][c] = bias * r.nextDouble();
 				}
-				F.put(n, Fu);
 				alreadyAdded.add(n);
 			}
 			community++;
@@ -128,41 +166,38 @@ public class main {
 
 		// Initialize weights W k,c
 		for (int attr = 0; attr < numAttr; attr++) {
-			W.put(attr, new ArrayList<Double>());
 			for (int com = 0; com < C; com++) {
-				W.get(attr).add((r.nextDouble() * 2.0) - 1.0);
+				W[attr][com] = (r.nextDouble() * 2.0) - 1.0;
 			}
 		}
 	}
 
 	// L_G
-	double graphLikelihood(HashMap<Integer, ArrayList<Double>> F) {
+	double graphLikelihood(double[][] F) {
 		double Lg = 0.0;
-		for (int u : G.keySet()) {
-			for (int v : G.keySet()) {
-				if (G.get(u).contains(v)) {
-					Lg += Math.log(1 - Math.exp(-1 * MathFunctions.dotProduct(F.get(u), F.get(v))));
-				} else
-					Lg -= MathFunctions.dotProduct(F.get(u), F.get(v));
+		for (int u = 0; u < G.size(); u++) {
+			for (int v : G.get(u)) {
+				if (G.get(u).contains(v))
+					Lg += Math.log(1 - Math.exp(-1 * MathFunctions.dotProduct(F[u], F[v])));
+				else
+					Lg -= MathFunctions.dotProduct(F[u], F[v]);
 			}
 		}
-
 		return Lg;
 	}
 
 	// Lx
-	double attributeLikelihood(HashMap<Integer, ArrayList<Double>> W, HashMap<Integer, ArrayList<Double>> F) {
+	double attributeLikelihood(double[][] W, double[][] F) {
 		double Lx = 0.0;
-		for (int u : G.keySet()) {
+		for (int u = 0; u < G.size(); u++) {
 			for (int k = 0; k < numAttr; k++) {
-				boolean x = X.get(u).get(k);
-				double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W.get(k), F.get(u))));
-
+				boolean x = X[u][k];
+				double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W[k], F[u])));
 				int z = x ? 1 : 0;
 				Lx += z * Math.log(Quk) + (1 - z) * Math.log(1 - Quk);
 				if (Double.isNaN(Lx)) {
 					System.out.println("Quk = " + Quk);
-					System.out.println(MathFunctions.dotProduct(W.get(k), F.get(u)));
+					System.out.println(MathFunctions.dotProduct(W[k], F[u]));
 					System.exit(0);
 				}
 			}
@@ -170,7 +205,7 @@ public class main {
 		return Lx;
 	}
 
-	double likelihood(HashMap<Integer, ArrayList<Double>> F, HashMap<Integer, ArrayList<Double>> W) {
+	double likelihood(double[][] F, double[][] W) {
 		return (1 - alpha) * graphLikelihood(F) + alpha * attributeLikelihood(W, F) - lambda * MathFunctions.L1Norm(W);
 	}
 
@@ -184,12 +219,11 @@ public class main {
 		boolean toContinue = true;
 		int iter = 0;
 		do {
-			HashMap<Integer, ArrayList<Double>> W_copy = new HashMap<>();
+			double[][] W_copy = new double[numAttr][C];
 			// System.out.println("t = " + t);
 			for (int k = 0; k < numAttr; k++) {
-				W_copy.put(k, new ArrayList<Double>());
 				for (int c = 0; c < C; c++) {
-					W_copy.get(k).add(W.get(k).get(c) + (t * updates.get(k).get(c)));
+					W_copy[k][c] = W[k][c] + (t * updates.get(k).get(c));
 				}
 			}
 
@@ -213,13 +247,12 @@ public class main {
 		int iter = 0;
 		boolean toContinue = true;
 		do {
-			HashMap<Integer, ArrayList<Double>> F_copy = new HashMap<>();
+			double[][] F_copy = new double[V][C];
 			boolean nonNegative = true;
 			// System.out.println("t = " + t);
-			for (int u : G.keySet()) {
-				F_copy.put(u, new ArrayList<Double>());
+			for (int u = 0; u < G.size(); u++) {
 				for (int c = 0; c < C; c++) {
-					F_copy.get(u).add(Math.max(0, F.get(u).get(c) + (t * updates.get(u).get(c))));
+					F_copy[u][c] = Math.max(0, F[u][c] + (t * updates.get(u).get(c)));
 				}
 			}
 
@@ -246,10 +279,10 @@ public class main {
 		double[] sumFvc = new double[C];
 		HashMap<Integer, ArrayList<Double>> updates = new HashMap<>();
 
-		for (int v : G.keySet()) {
-			ArrayList<Double> Fv = F.get(v);
+		for (int v = 0; v < G.size(); v++) {
+			double[] Fv = F[v];
 			for (int c = 0; c < C; c++) {
-				sumFvc[c] += Fv.get(c);
+				sumFvc[c] += Fv[c];
 			}
 		}
 		ArrayList<ArrayList<Integer>> nodeSplits = new ArrayList<>();
@@ -257,7 +290,7 @@ public class main {
 			nodeSplits.add(new ArrayList<>());
 
 		int i = 0;
-		for (int node : G.keySet()) {
+		for (int node = 0; node < G.size(); node++) {
 			nodeSplits.get(i % THREADS).add(node);
 			i++;
 		}
@@ -286,15 +319,14 @@ public class main {
 			}
 		}
 
-		eta = lineSearch(updates);
+		// eta = lineSearch(updates);
 		System.out.println("Final eta " + eta);
-
-		for (int u : updates.keySet()) {
+		
+		for (int u = 0; u < G.size(); u++) {
 			for (int c = 0; c < C; c++) {
-				F.get(u).set(c, Math.max(0.0, F.get(u).get(c) + eta * updates.get(u).get(c)));
+				F[u][c] = Math.max(0.0, F[u][c] + eta * updates.get(u).get(c));
 			}
 		}
-
 	}
 
 	class UpdateFThread extends Thread {
@@ -316,7 +348,6 @@ public class main {
 	class UpdateWThread extends Thread {
 
 		ArrayList<Integer> attributes;
-
 		HashMap<Integer, ArrayList<Double>> updates;
 
 		UpdateWThread(ArrayList<Integer> attributes) {
@@ -345,31 +376,30 @@ public class main {
 			double[] derivative_X = new double[C];
 			double[] diff = new double[C];
 			for (int v : G.get(u)) {
-
-				double dot = MathFunctions.dotProduct(F.get(u), F.get(v));
+				double dot = MathFunctions.dotProduct(F[u], F[v]);
 				double exp = Math.exp(-1 * dot);
 				double fraction = exp / (1 - exp);
 
 				for (int c = 0; c < C; c++) {
-					derivative_G[c] += F.get(v).get(c) * fraction;
-					diff[c] -= F.get(v).get(c);
+					derivative_G[c] += F[v][c] * fraction;
+					diff[c] -= F[v][c];
 					// derivative_G[c] -= F.get(v).get(c);
 				}
 
 			}
 			for (int c = 0; c < C; c++) {
-				diff[c] = diff[c] + sumFvc[c] - F.get(u).get(c);
+				diff[c] = diff[c] + sumFvc[c] - F[u][c];
 				derivative_G[c] -= diff[c];
 			}
 
 			// derivative of Fu done
 
 			for (int k = 0; k < numAttr; k++) {
-				int z = X.get(u).get(k) ? 1 : 0;
-				double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W.get(k), F.get(u))));
+				int z = X[u][k] ? 1 : 0;
+				double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W[k], F[u])));
 				double difference = (z - Quk);
 				for (int c = 0; c < C; c++) {
-					derivative_X[c] += difference * W.get(k).get(c);
+					derivative_X[c] += difference * W[k][c];
 				}
 			}
 
@@ -384,29 +414,30 @@ public class main {
 		return updates;
 	}
 
+	// TODO: Function not in use.
 	void updateW() {
 		HashMap<Integer, ArrayList<Double>> newW = new HashMap<>();
 
 		for (int k = 0; k < numAttr; k++) {
 			newW.put(k, new ArrayList<Double>());
 			for (int c = 0; c < C; c++) {
-				for (int u : G.keySet()) {
-					double z = X.get(u).get(k) ? 1 : 0;
-					double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W.get(k), F.get(u))));
+				for (int u = 0; u < G.size(); u++) {
+					double z = X[u][k] ? 1 : 0;
+					double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W[k], F[u])));
 					double diff = (z - Quk);
 					double update = 0.0;
-					update += diff * F.get(u).get(c);
+					update += diff * F[u][c];
 					update *= alpha;
-					if (W.get(k).get(c) > 0)
+					if (W[k][c] > 0)
 						update -= lambda;
 					else
 						update += lambda;
 					newW.get(k).add(update * eta);
+
 				}
 			}
 		}
-
-		W = new HashMap<>(newW);
+		// W = new HashMap<>(newW);
 	}
 
 	HashMap<Integer, ArrayList<Double>> updateW(ArrayList<Integer> attributes) {
@@ -414,14 +445,14 @@ public class main {
 		for (int k : attributes) {
 			updates.put(k, new ArrayList<Double>());
 			for (int c = 0; c < C; c++) {
-				for (int u : G.keySet()) {
-					double z = X.get(u).get(k) ? 1 : 0;
-					double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W.get(k), F.get(u))));
+				for (int u = 0; u < G.size(); u++) {
+					double z = X[u][k] ? 1 : 0;
+					double Quk = 1 / (1 + Math.exp(-1 * MathFunctions.dotProduct(W[k], F[u])));
 					double diff = (z - Quk);
 					double update = 0.0;
-					update += diff * F.get(u).get(c);
+					update += diff * F[u][c];
 					update *= alpha;
-					if (W.get(k).get(c) > 0)
+					if (W[k][c] > 0)
 						update -= lambda;
 					else
 						update += lambda;
@@ -457,20 +488,18 @@ public class main {
 
 		for (UpdateWThread thread : threads) {
 			for (int k : thread.updates.keySet()) {
-				updates.put(k, new ArrayList());
+				updates.put(k, new ArrayList<Double>());
 				for (int c = 0; c < C; c++) {
 					updates.get(k).add(thread.updates.get(k).get(c));
-					// W.get(k).set(c, W.get(k).get(c) + eta *
-					// thread.updates.get(k).get(c));
 				}
 			}
 		}
-		eta = lineSearchW(updates);
+		// eta = lineSearchW(updates);
 		// eta = 0.0001;
-
-		for (int k : updates.keySet()) {
+		
+		for (int k = 0 ; k < numAttr; k ++) {
 			for (int c = 0; c < C; c++) {
-				W.get(k).set(c, W.get(k).get(c) + eta * updates.get(k).get(c));
+				W[k][c] = W[k][c] + eta * updates.get(k).get(c);
 			}
 		}
 
@@ -492,7 +521,8 @@ public class main {
 			previousLikelihood = likelihood;
 
 			updateFDriver();
-			// System.out.println("Likelihood after F update at iter " + iter + " " + likelihood(F, W));
+			// System.out.println("Likelihood after F update at iter " + iter +
+			// " " + likelihood(F, W));
 			updateWDriver();
 			// updateW();
 
@@ -508,9 +538,9 @@ public class main {
 			communities.add(new ArrayList<>());
 		}
 
-		for (int u : G.keySet()) {
+		for (int u = 0; u < G.size(); u++) {
 			for (int c = 0; c < C; c++) {
-				if (F.get(u).get(c) > delta) {
+				if (F[u][c] > delta) {
 					communities.get(c).add(u);
 				}
 			}
@@ -519,7 +549,7 @@ public class main {
 		for (ArrayList<Integer> comm : communities) {
 			System.out.print("Circle");
 			for (int x : comm)
-				System.out.print(" " + x);
+				System.out.print(" " + nodeIdMap.get(x));
 			System.out.println();
 		}
 	}
@@ -533,15 +563,17 @@ public class main {
 		// String attrfilename = "facebook/0.feat";
 		readGraph(graphfilename, attrfilename);
 		initAffiliations();
+		
 		gradientAscent();
 		getCommunities();
-		return likelihood(F, W);
+		return 0.0;
+		// return likelihood(F, W);
 	}
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		main m = new main(10);
-		m.driver("facebook/107.edges", "facebook/107.feat");
+		m.driver("facebook/0.edges", "facebook/0.feat");
 
 	}
 
